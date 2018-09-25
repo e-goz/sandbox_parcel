eb5a768129b8653130aea917ea98010d71b7d718
guillaume <egoz67@gmail.com>
2018-09-26 00:20:30 +0200

    First commit

---
+++ src/mynodemodules.js
@@ -0,0 +1,8 @@
+window.$ = require("jquery");
+window._ = require("lodash");
+
+require("datatables.net-bs4")();
+
+require("./scripts.js");
+
+import "./styles.scss";
---
+++ src/scripts.js
@@ -0,0 +1,6 @@
+const sayHello = () => "Hello World";
+
+const myArray = [1, 2];
+
+console.log(myArray.includes(1));
+console.log(sayHello());
